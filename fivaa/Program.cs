e6bbb56e96debe3c293e050b047526d765edd091
Reza Fiansyah Putra <rezafiansyah.putra@gmail.com>
2018-10-13 12:20:27 +0700

    Commit

---
+++ fivaa/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace fivaa
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Fivaa(5);
+            Console.ReadLine();
+        }
+
+        private static void Fivaa(int number)
+        {            
+            for(int i = number; i > 0; i--)
+            {
+                string prefix = (number-1).ToString();
+                string postfix = (number+1).ToString();
+                Console.Write(string.Concat(prefix, prefix));                
+                for (int j = 0; j < number; j++)
+                {                    
+                    Console.Write(postfix);
+                }
+                Console.WriteLine();
+                number--;
+            }
+        }
+
+    }
+}
